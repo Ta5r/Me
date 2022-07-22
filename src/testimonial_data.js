a17415bf97e64ec63c405943f64c3ed8379fbe1a
Ta5r <srv.tanay@gmail.com>
2022-07-22 12:29:24 +0530

    testimonial d1vshar, kg-kartik updated

--- src/testimonial_data.js
+++ src/testimonial_data.js
@@ -1,7 +1,7 @@
 const testimonial_data = [
   {
     heading: 'Heading XYZ',
-    text: 'vhb bhbvb vv fn vldfv dfbv jkv  v bhd v vnv va b vhj    nasvhv dhv dhv j bvd jh  v',
+    text: 'I have worked with Tanay in 2-3 projects and must say he is quite persistent in his work. He always seeks to learn new things and enhance his skills. I wish him good luck for his future endeavors!',
     img: 'https://avatars.githubusercontent.com/u/24389474?v=4',
     name: 'Kartik Goel',
     title: 'Full Stack Web Developer',
@@ -16,8 +16,8 @@ const testimonial_data = [
     link: 'https://github.com/manavendrasen',
   },
   {
-    heading: 'Heading XYZ',
-    text: 'vhb bhbvb vv fn vldfv dfbv jkv  v bhd v vnv va b vhj    nasvhv dhv dhv j bvd jh  v',
+    heading: 'Tanay is very passionate about technology.',
+    text: 'I have worked with him for over 4 months and found him to be very hard working. He accomplished all tasks and constantly pushed himself and others around him to learn more. I wish him luck and thank him for his contributions to IEEE MUJ!',
     img: 'https://avatars.githubusercontent.com/u/32746859?v=4',
     name: 'Divyanshu Sharma',
     title: 'Full Stack Web Developer',
